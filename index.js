12e7e921b848acd6d15540ed7bd80601723e8a77
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-11-07 11:22:21 +0100

    Added README

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-"// Index File" 
+/*
+This is the primary file for the API
+*/
+
+//Dependecies
+const http = require('http');
+const url = require('url');
+
+//Create server to respond to requests with a string
+const server = http.createServer((req, res) => {
+    res.end("Hello World\n");
+})
+
+// Fire up server
+let port = 3000;
+server.listen(port, ()=>{
+    console.log("Server is live and listening on port ", port);
+})
